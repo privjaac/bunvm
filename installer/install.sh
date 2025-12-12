b6061c6b8d85a077966b2312da3b11f0d77e1dde
jaac <alex_alvarezcasas@hotmail.com>
2025-12-12 00:18:29 -0500

    main: add installer folders for cloudflared-pages

---
+++ installer/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "🔧 Instalando BunVM..."
+
+INSTALLER_URL="https://raw.githubusercontent.com/privjaac/bunvm/main/install.sh"
+
+curl -fsSL "$INSTALLER_URL" | bash
