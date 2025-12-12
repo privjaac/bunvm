75e2c289729fe6311ead59783002b98ed9bb5375
jaac <alex_alvarezcasas@hotmail.com>
2025-12-12 01:08:53 -0500

    main: update headers and remove unused install script

--- public/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "🔧 Instalando BunVM..."
-
-INSTALLER_URL="https://raw.githubusercontent.com/privjaac/bunvm/main/install.sh"
-
-curl -fsSL "$INSTALLER_URL" | bash
